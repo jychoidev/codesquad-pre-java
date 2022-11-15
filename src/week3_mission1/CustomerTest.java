7197a107e529e0beb0d27c2b6e04dbc741eadc82
Jaeyoung Choi <jaeyoungchoi.tech@gmail.com>
2022-11-15 16:25:23 +0900

    Member 클래스 파일, CustomerTest 클래스 파일 수정
    금액계산, 고객정보 출력 메소드에 매개변수 shoppingPrice 넣어 수정함.

--- src/week3_mission1/CustomerTest.java
+++ src/week3_mission1/CustomerTest.java
@@ -4,18 +4,18 @@ public class CustomerTest {
 
     public static void main(String[] args) {
 
-        Red customer1 = new Red("James", 10000, 2);
-        Red customer2 = new Red("Tomas", 30000, 2);
-        Platinum customer3 = new Platinum("Edward", 10000, 2);
-        Platinum customer4 = new Platinum("Percy", 30000, 2);
-        Diamond customer5 = new Diamond("Elizabet", 30000, 2);
+        Red customer1 = new Red("James", 0, 2);
+        Red customer2 = new Red("Tomas", 0, 2);
+        Platinum customer3 = new Platinum("Edward", 0, 2);
+        Platinum customer4 = new Platinum("Percy", 0, 2);
+        Diamond customer5 = new Diamond("Elizabet", 0, 2);
 
 
-        System.out.println(customer1.showCustomerInfo());
-        System.out.println(customer2.showCustomerInfo());
-        System.out.println(customer3.showCustomerInfo());
-        System.out.println(customer4.showCustomerInfo());
-        System.out.println(customer5.showCustomerInfo());
+        System.out.println(customer1.showCustomerInfo(10000));
+        System.out.println(customer2.showCustomerInfo(30000));
+        System.out.println(customer3.showCustomerInfo(10000));
+        System.out.println(customer4.showCustomerInfo(30000));
+        System.out.println(customer5.showCustomerInfo(30000));
 
     }
 }
--- src/week3_mission1/Member.java
+++ src/week3_mission1/Member.java
@@ -20,14 +20,14 @@ public class Member {
     }
 
     // 금액 계산 메소드
-    public int calcPrice() {
+    public int calcPrice(int shoppingPrice) {
         bonusPoint = (int)(shoppingPrice * bonusRatio); //적립 포인트
         return shoppingPrice - (int)(shoppingPrice * discountRatio); //지불 금액
     }
 
     // 고객정보 출력 메소드
-    public String showCustomerInfo() {
-        return customerName + "님의 지불 금액은 " + calcPrice() + "원이고, 적립 포인트는 "
+    public String showCustomerInfo(int shoppingPrice) {
+        return customerName + "님의 지불 금액은 " + calcPrice(shoppingPrice) + "원이고, 적립 포인트는 "
                 + bonusPoint + "점 입니다. " + "\r\n" + "주차 요금은 " + parkingFee + "원 입니다.";
     }
 
